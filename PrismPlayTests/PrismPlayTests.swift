32080d48939f5d7946250f9830afc33ae351a514
Ayanrocks <ayanb1999@gmail.com>
2025-12-07 00:47:54 +0530

    Initial Commit

---
+++ PrismPlayTests/PrismPlayTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrismPlayTests.swift
+//  PrismPlayTests
+//
+//  Created by Ayan Banerjee on 07/12/25.
+//
+
+import Testing
+@testable import PrismPlay
+
+struct PrismPlayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
